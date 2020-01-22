96f5782c73a006292b1ab9627eee5a9aa0137493
Ignacio Rodríguez <igrodray@gmail.com>
2020-01-22 17:51:41 +0100

    Correct CORS configuration

--- config/application.rb
+++ config/application.rb
@@ -34,15 +34,11 @@ module AppChat
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
-        resource(
-          '*',
-          headers: :any,
-          methods: [:get, :patch, :put, :delete, :post, :options]
-          )
-      end
+         origins '*'
+         resource '*', :headers => :any, :methods => [:get, :post, :options]
+       end
     end
   end
 end
