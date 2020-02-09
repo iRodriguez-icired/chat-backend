785ff4364e4404a08f8b0243e5509c735c84e36b
NachoRay <igrodray@gmail.com>
2020-02-09 23:38:47 +0100

    Add a test for room

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -11,4 +11,9 @@ RSpec.describe Room, type: :model do
     @room = Room.new(name: @saved_room.name)
     expect(@room.valid?).to eq false
   end
+
+  it 'if name is valid and unique, room is valid' do
+    @room = Room.new(name: 'Sala 1')
+    expect(@room.valid?).to eq true
+  end
 end
