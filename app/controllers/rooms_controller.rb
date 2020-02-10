ac04d195b8d2739e450bb97f8adae370607eba44
iRodriguez-icired <irodriguez@iciredimpagados.com>
2020-02-10 11:20:34 +0100

    Securize rooms_controller params

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,7 +5,7 @@ class RoomsController < ApplicationController
   end
 
   def create
-    @room = Room.new(name: params[:name])
+    @room = Room.new(name: create_params[:name])
     if @room.save
       render json: {room: @room}, status: 201
     else
@@ -14,9 +14,19 @@ class RoomsController < ApplicationController
   end
 
   def show
-    id = params[:id]
+    id = show_params[:id]
     messages = Message.where(room_id: id).to_a
     messages = messages.reverse.take(20)
     render json: {messages: messages}, status: 200
+  end
+
+  private
+
+  def show_params
+    params.permit(:id)
+  end
+
+  def create_params
+    params.permit(:name)
   end
 end
