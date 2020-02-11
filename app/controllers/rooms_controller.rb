9b018f2de05464c3ca17b07e7ea94a80a1bf9f9c
iRodriguez-icired <irodriguez@iciredimpagados.com>
2020-02-11 17:55:38 +0100

    Minor format change

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -9,12 +9,10 @@ class RoomsController < ApplicationController
     uniqueness = Room.find_by(name: @room.name).nil?
     if @room.save
       render json: {room: @room}, status: 201
+    elsif uniqueness
+      render json: {message: I18n.t('room_creation_failed')}, status: 422
     else
-      if uniqueness
-        render json: {message: I18n.t('room_creation_failed')}, status: 422
-      else
-        render json: {message: I18n.t('room_creation_failed_not_unique')}, status: 422
-      end
+      render json: {message: I18n.t('room_creation_failed_not_unique')}, status: 422
     end
   end
 
